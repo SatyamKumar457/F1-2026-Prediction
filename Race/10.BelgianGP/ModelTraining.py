b86fde81281a96d84debb6522e7723d8222bbe63
SatyamKumar457 <satyamkumar1092004@gmail.com>
2026-07-17 22:52:05 +0530

    Model Training.

---
+++ Race/10.BelgianGP/ModelTraining.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import StandardScaler
+from scipy.stats import spearmanr
+from sklearn.metrics import make_scorer
+import random
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+
+File_Path = "Race/10.BelgianGP/"
+
+TRA = pd.read_csv(f"{File_Path}Data/TrainingData.csv")
+
+columns = [
+       'AveragePointsFromLast3Races', 'ConstructorAveragePointFromLast3Races',
+       'FP1_BestTime(s)', 'FP2_BestTime(s)', 'FP3_BestTime(s)','Qualifying_Time(s)', 
+       'Starting_Pos', 'DriverPoints', 'ConstructorPoints', 'FP1_Rank',
+       'FP2_Rank', 'FP3_Rank', 'FP1_DeltaToFastest', 'FP2_DeltaToFastest',
+       'FP3_DeltaToFastest','StartXConst', 'DriXConst', 'FP3XStart'
+       ]
+
+X = TRA[columns]
+
+y = TRA['Race_Result']
+
+X_train, X_test, y_train, y_test = train_test_split(X , y, test_size = 0.25, random_state = 23)
+
+model = Ridge()
+
+
+param_grid = {
+    "alpha": [ 0.01, 0.1, 1, 10, 25, 50, 100, 250, 500, 1000, 5000, 10000],
+    "fit_intercept": [True, False],
+    "solver": ["auto", "svd", "cholesky", "lsqr"],
+    "max_iter":[100,200,500,1000,10000,100000],
+    "positive": [True,False],
+    "random_state":[42]
+}
+
+def spearman_correlation(y_true,y_pred):
+    return spearmanr(y_true,y_pred)[0]
+
+spearman_scorer = make_scorer(spearman_correlation, greater_is_better=True)
+
+
+regcv = GridSearchCV(
+    model,
+    param_grid,
+    cv=5,
+    scoring=spearman_scorer
+)
+
+regcv.fit(X_train, y_train)
+
+y_pred = regcv.predict(X_test)
+
+
+corr, _ = spearmanr(y_pred, y_test)
+print("Spearman Rank:", corr)
+
+
+joblib.dump(regcv, f"{File_Path}Model/Ridge{corr:.2f}.pkl")
+
+
+best_model = regcv.best_estimator_
+
+feature_importance = pd.Series(
+    best_model.coef_,
+    index=X.columns
+)
+
+feature_importance.sort_values(key=abs, ascending=False)
+features = X.columns
+
+plt.figure(figsize=(10,6))
+plt.barh(features, feature_importance, color='skyblue')
+plt.xlabel("Importance")
+plt.title("Feature Importance After Feature Selection", fontsize=16, fontweight='bold', pad=15)
+plt.tight_layout()
+plt.savefig(f"{File_Path}Plots/Feature_importance.png", dpi=300, bbox_inches='tight')
